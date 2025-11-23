487c59e241e8eb6599de1ef6534f5910e1391c26
Artem Lymarenko <artemchiklym@gmail.com>
2025-11-23 02:49:46 +0200

    notification

--- services/notification-service/cmd/main.go
+++ services/notification-service/cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"soa-video-streaming/pkg/httpsrv"
 	"soa-video-streaming/pkg/rabbitmq"
 	"soa-video-streaming/services/notification-service/internal/config"
 	"soa-video-streaming/services/notification-service/internal/service"
@@ -15,7 +14,6 @@ func main() {
 
 	fx.New(
 		config.Module(),
-		httpsrv.Module(),
 		rabbitmq.Module(),
 		service.Module(),
 	).Run()
